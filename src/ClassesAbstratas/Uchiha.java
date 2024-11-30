6c9aa3fe75ce5ef9edaa0bf5c4597c6568990bc4
guiemiliano <guiemiliano.dev@gmail.com>
2024-11-29 21:47:42 -0300

    Ampliando ainda mais meus conhecimentos em POO com Java!

--- src/ClassesAbstratas/Uchiha.java
+++ src/ClassesAbstratas/Uchiha.java
@@ -9,6 +9,7 @@ public class Uchiha extends Ninja{
         super(nome, aldeia, idade);
     }
 
+    //Método do segundo pilar de OOP, Polimorfismo
     @Override
     public void nomeDoNinja() {
         System.out.println("Meu nome é : " + nome + " método da classe UCHIHA");
